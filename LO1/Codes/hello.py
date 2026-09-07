58f73ac4f865749a0a298f2cc887034699f12f74
Darren <2603839@sit.singaporetech.edu.sg>
2026-09-07 16:08:30 +0800

    Add INF1103 Lab Files

---
+++ LO1/Codes/hello.py
@@ -0,0 +1,57 @@
+#Activity 1
+
+print("=======")
+print("Welcome Here")
+print("My first Post!")
+print("=======")
+
+#Activity 2
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+#Activity 3
+
+followers = 100
+
+followers += 50
+print("Day 1", followers)
+
+followers += 20
+print("Day 2", followers)
+
+followers -= 10
+print("Day 3", followers)
+
+#Activity 4
+
+username = input("Enter your username: ")
+age = int(input("Enter your age: "))
+category = input("Enter your category: ")
+
+print("\nInstagram Profile")
+print("===================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+#Activity 5
+
+username = input("Enter your username: ")
+age = int(input("Enter your age: "))
+category = input("Enter your category: ")
+
+print("\nInstagram Profile")
+print("===================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+if (age > 40 and category == "fun"):
+    print("You are too old what is fun for you??")
+
