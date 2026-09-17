270b9031b693af9eb3f5876270cf5199d59a81da
Darren <2603839@sit.singaporetech.edu.sg>
2026-09-18 00:15:07 +0800

    Completed get_valid_input()

---
+++ LO3/Codes/auditor.py
@@ -0,0 +1,58 @@
+#pre defined variables
+
+stock_quantity_recorded = 0
+rejects = 0
+
+#pre defined functions
+
+def get_valid_input():
+
+    while(True):
+
+        stock_quantity = input("Enter the quantity: ")
+
+        if (stock_quantity.lower() == 'quit'):
+            return "quit"
+
+        try:
+
+            stock_quantity = int(stock_quantity)
+
+            if (stock_quantity < 0):
+                print("Please input a positive value.")
+                return 0
+
+            elif (stock_quantity >= 500):
+                print(f"Stock Quantity inputted is {stock_quantity} which exceeded the limit of 500")
+                return 0
+                
+
+            elif (stock_quantity > 0 and stock_quantity < 500):
+                return stock_quantity
+
+            else:
+                print("Unknown Error")
+                return 0
+
+        except ValueError:
+            print("Invalid Input. Please input values only.")
+            return 0
+
+
+#main code
+
+while(True):
+
+    result = get_valid_input()
+
+    if (result == 'quit'):
+        print("Total Quantity Recorded: ", stock_quantity_recorded)
+        print("Number of Failed Entry: ", rejects)
+        print("Program End")
+        break
+
+    elif (result == 0):
+        rejects += 1
+
+    elif (result > 0):
+        stock_quantity_recorded += result
